fbdd9c601fa6f1e2f492460751c824f989059f87
Thommy Calderon <thommy.calderon@udla.edu.ec>
2023-06-28 16:39:32 -0500

    torneo

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<string.h>
 
+int main (){
+    int finaliza;
+    char termina[]="cerrar";
+    int numequipo=0;
+    char nombre[20];
+    char equipos[100];
+    do {
+    printf("Ingrese el nombre del equipo\n");
+    scanf("%s",nombre);
+    finaliza=strcmp(termina, nombre);
+    if (finaliza!=0)
+        numequipo = numequipo + 1;
+    }
+    while (finaliza!=0);
+
+
+}
